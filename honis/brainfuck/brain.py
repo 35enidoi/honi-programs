692815d01681bd0d55c9853a6ce474bbef31f00c
いおぢね <143810964+35enidoi@users.noreply.github.com>
2024-02-23 23:22:23 +0900

    #3 静的解析の関数化

--- honis/brainfuck/brain.py
+++ honis/brainfuck/brain.py
@@ -48,25 +48,29 @@ def brain(code:str,
 
         return bracketslike[index][1]
 
-    brackets = [(i, v) for v, i in enumerate(coded) if i == "[" or i == "]"]
+    def static_bracket_analysis(bracketslike:list[tuple[str, int]], openb:str, closeb:str) -> None:
+        if len(bracketslike) != 0:
+            # まずブラケットがあるコードなのか(index out of range対策)
+            if (openb_c := (brlist := tuple(map(lambda x:str(x[0]), bracketslike))).count(openb))+(closeb_c := brlist.count(closeb)) != len(brlist):
+                # bracketslikeに異物混入していた場合
+                raise ValueError("static analysis fail:bracketlike include other object")
+            if openb_c != closeb_c:
+                # openbとclosebの量が違う
+                if openb_c-closeb_c > 0:
+                    # closebの量が多い
+                    raise CloseBracketError(f"too many {closeb}")
+                else:
+                    # openbの量が多い
+                    raise OpenBracketError(f"too many {closeb}")
+            elif brlist[0] == closeb:
+                # 初めに閉じるブラケットのある無効なプログラムである
+                raise CloseBracketError("Invalid program")
+            elif brlist[-1] == openb:
+                # 最後に開くブラケットのある無効なプログラム
+                raise OpenBracketError("Invalid program")
 
-    # 角括弧の例外をすべてここでキャッチする
-    if len(brackets) != 0:
-        # まずブラケットがあるコードなのか(index out of range対策)
-        if (brlist := tuple(map(lambda x:str(x[0]), brackets))).count("[") != brlist.count("]"):
-            # `[`と`]`の量が違う
-            if brlist.count("[")-brlist.count("]") > 0:
-                # `]`の量が多い
-                raise CloseBracketError("too many ]")
-            else:
-                # `[`の量が多い
-                raise OpenBracketError("too many [")
-        elif brlist[0] == "]":
-            # 初めに閉じるブラケットのある無効なプログラムである
-            raise CloseBracketError("Invalid program")
-        elif brlist[-1] == "[":
-            # 最後に開くブラケットのある無効なプログラム
-            raise OpenBracketError("Invalid program")
+    brackets = [(i, v) for v, i in enumerate(coded) if i == "[" or i == "]"]
+    static_bracket_analysis(brackets)
 
     if stepmode or debug:
         print("\n"+code.strip()+"\n")
